fafc449784c13b0327177e3d81115fe4e8a98506
Mrsaint001 <blessingumeh80@gmail.com>
2026-07-10 14:54:31 -0700

    added share button mobile fix

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ url: window.location.href
 });
 
 }
+function sharePortfolio(){
+
+    if(navigator.share){
+
+        navigator.share({
+
+            title: "SAINT - Game Developer Portfolio",
+
+            text: "Check out my game developer portfolio!",
+
+            url: window.location.href
+
+        });
+
+    } else {
+
+        alert("Copy my portfolio link and share it!");
+
+    }
+
+}
