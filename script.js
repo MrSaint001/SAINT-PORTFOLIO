622fe32c160b7cf19ef0b9cef7ef41d3a8604dd0
Mrsaint001 <blessingumeh80@gmail.com>
2026-07-10 14:48:07 -0700

    added sharing feature

---
+++ script.js
@@ -0,0 +1,13 @@
+function sharePortfolio(){
+
+navigator.share({
+
+title: "SAINT - Game Developer Portfolio",
+
+text: "Check out my game developer portfolio!",
+
+url: window.location.href
+
+});
+
+}
