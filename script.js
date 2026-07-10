3151de88752d290926876577936ad56262520f76
Mrsaint001 <blessingumeh80@gmail.com>
2026-07-10 15:00:45 -0700

    fixed mobile button clicks

--- script.js
+++ script.js
@@ -13,21 +13,20 @@ url: window.location.href
 }
 function sharePortfolio(){
 
-    if(navigator.share){
+    if (navigator.share) {
 
         navigator.share({
-
             title: "SAINT - Game Developer Portfolio",
-
             text: "Check out my game developer portfolio!",
-
             url: window.location.href
-
-        });
+        })
+        .catch(error => console.log(error));
 
     } else {
 
-        alert("Copy my portfolio link and share it!");
+        navigator.clipboard.writeText(window.location.href);
+
+        alert("Portfolio link copied!");
 
     }
 
